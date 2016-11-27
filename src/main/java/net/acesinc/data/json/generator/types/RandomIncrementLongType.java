7e03e28b579e25c57604526974b7748b27e85538
Ygal Blum <ygal.blum@gmail.com>
2016-11-27 10:10:00 +0200

    Add support for memory and multiple instances

--- src/main/java/net/acesinc/data/json/generator/types/RandomIncrementLongType.java
+++ src/main/java/net/acesinc/data/json/generator/types/RandomIncrementLongType.java
@@ -5,6 +5,9 @@
  */
 package net.acesinc.data.json.generator.types;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author ygalblum
@@ -13,43 +16,73 @@ public class RandomIncrementLongType extends TypeHandler {
     public static final String TYPE_NAME = "randomIncrementLong";
     public static final String TYPE_DISPLAY_NAME = "Random Increment Long";
 
-    private long nextValue;
-    private long minStep;
-    private long maxStep;
+    private class IncrementParameters {
+        public long nextValue;
+        public final long minStep;
+        public final long maxStep;
+
+        public IncrementParameters(long nextValue, long minStep, long maxStep) {
+            this.nextValue = nextValue;
+            this.minStep = minStep;
+            this.maxStep = maxStep;
+        }
+    };
+
+    private String currentRandomIncrementLongName;
+    private Map<String, IncrementParameters> namedRandomIncrementLongMap;
+
+    public RandomIncrementLongType(){
+        namedRandomIncrementLongMap = new HashMap<>();
+    }
     
     @Override
     public void setLaunchArguments(String[] launchArguments) {
+        if (launchArguments.length < 1) {
+            return;
+        }
+        currentRandomIncrementLongName = launchArguments[0];
+        if (namedRandomIncrementLongMap.get(currentRandomIncrementLongName) != null) {
+            return;
+        }
+
+        long nextValue;
+        long minStep;
+        long maxStep;
+
         super.setLaunchArguments(launchArguments);
         switch (launchArguments.length) {
-            case 0:
+            case 1:
                 nextValue = 0;
                 minStep = 0;
                 maxStep = Long.MAX_VALUE;
                 break;
-            case 1:
-                nextValue = Long.parseLong(launchArguments[0]);
+            case 2:
+                nextValue = Long.parseLong(launchArguments[1]);
                 minStep = 0;
                 maxStep = Long.MAX_VALUE;
                 break;
-            case 2:
-                nextValue = Long.parseLong(launchArguments[0]);
-                minStep = Long.parseLong(launchArguments[1]);
+            case 3:
+                nextValue = Long.parseLong(launchArguments[1]);
+                minStep = Long.parseLong(launchArguments[2]);
                 maxStep = Long.MAX_VALUE;
                 break;
-            case 3:
-                nextValue = Long.parseLong(launchArguments[0]);
-                minStep = Long.parseLong(launchArguments[1]);
-                maxStep = Long.parseLong(launchArguments[2]);
+            case 4:
+                nextValue = Long.parseLong(launchArguments[1]);
+                minStep = Long.parseLong(launchArguments[2]);
+                maxStep = Long.parseLong(launchArguments[3]);
                 break;
             default:
-                break;
+                return;
         }
+
+        namedRandomIncrementLongMap.put(currentRandomIncrementLongName, new IncrementParameters(nextValue, minStep, maxStep));
     }
     
     @Override
     public Object getNextRandomValue() {
-        long value = nextValue;
-        nextValue += getRand().nextLong(minStep, maxStep);
+        IncrementParameters incrementParameters = namedRandomIncrementLongMap.get(currentRandomIncrementLongName);
+        long value = incrementParameters.nextValue;
+        incrementParameters.nextValue += getRand().nextLong(incrementParameters.minStep, incrementParameters.maxStep);
         return value;
     }
 
